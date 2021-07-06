c4288b1fdea5a971163a4c36a7234724569982dd
nikob <nikolaj.bauer@uzh.ch>
2021-07-06 11:51:13 +0200

    add Terztsausch

--- Dandrieu_rules.py
+++ Dandrieu_rules.py
@@ -1,25 +1,42 @@
 import music21 as m21
+from typing import List
 from music21.figuredBass import rules
 from music21.figuredBass import examples
 from music21.figuredBass import realizer
 
 
-def dandrieu_octave_rule(notes, keySig: m21.key.Key,
+def dandrieu_octave_rule(notes: List[m21.note.Note], keySig: m21.key.Key,
                          timeSig: m21.meter.TimeSignature) -> m21.figuredBass.realizer.FiguredBassLine:
-    """creates a composition after Dandrieu's Octave rule"""
+    """ applies Dandrieus Rules to harmonize a bass line.
+    :ivar: a list of bass notes as well as key and time signature
+    :return: a ~music21.figuredBass.realizer.FiguredBassLine object"""
     # keySig = m21.key.Key(key, mode)
     fbLine = realizer.FiguredBassLine(keySig, timeSig)  # create a fbLine object
 
     if keySig.mode == 'major':
-        dandrieu_rules = {1: '', (2, 3): '6,4,3', 3: '6', (4, 5): '6,5',
-                          5: '', (6, 7): '6', (7, 1): '5,6,3', (7, 6): '6',
-                          (6, 5): '#6,4,3', (4, 3): '4,6,2', (2, 1): '6,4,3'}
+        dandrieu_rules = {
+            1: '', 5: '',  # the naturel (empty string defaults to 3,5)
+            (2, 3): '6,4,3', (2, 1): '6,4,3', (6, 5): '#6,4,3',  # petite sixte
+            3: '6', (6, 7): '6', (7, 6): '6',  # sixte simple / sixte doublee
+            (4, 5): '6,5', # quinte et sixte
+            (7, 1): '5,6,3', # fausse quinte
+            (4, 3): '4,6,2',  # l'accord de tritone
+            (6, 4): '3,6,4', # Terztausch
+            
+              }
+
 
     elif keySig.mode == 'minor':
         # TODO: implement minor
-        dandrieu_rules = {1: '', (2, 3): '#6,4,3', 3: '6', (4, 5): '6,5',
-                          5: '#3,5,8', (6, 7): '6', (7, 1): '5,6,3', (7, 6): '6',
-                          (6, 5): '6,4,3', (4, 3): '#4,2,6', (2, 1): '#6,4,3'}
+        dandrieu_rules = {
+            1: '', 5: '#3,5,8',
+            (2, 3): '#6,4,3', (6, 5): '6,4,3', (2, 1): '#6,4,3',
+            3: '6', (6, 7): '6', (7, 6): '6',
+            (4, 5): '6,5',
+            (7, 1): '5,6,3',
+            (4, 3): '#4,2,6',
+            (6, 4): '3,6,4',  # Terztausch
+        }
 
     else:
         # TODO:
@@ -36,7 +53,6 @@ def dandrieu_octave_rule(notes, keySig: m21.key.Key,
             this_note_degree = keySig.getScaleDegreeAndAccidentalFromPitch(bass_note.pitch)[0]
             # have to use the long one, getting also the accidental, because of melodic minor...
             following_note_degree = keySig.getScaleDegreeAndAccidentalFromPitch(following_note.pitch)[0]
-            print(bass_note, this_note_degree)
 
             if this_note_degree in dandrieu_rules:
                 fbLine.addElement(bass_note, dandrieu_rules[this_note_degree])
@@ -52,13 +68,18 @@ def dandrieu_octave_rule(notes, keySig: m21.key.Key,
 
 
 if __name__ == '__main__':
-    # create example
+    # create examples:
+    # scales
     c_major = "C1 D E F G A B c c B A G F E D C"
     d_major = "D1 E F# G A B c# d d c# B A G F# E D"
     d_minor = 'D1 E F G A B c# d d c B- A G F E D'
     c_minor = 'C1 D E- F G A B c c B- A- G F E- D C'
+
+    # special movements:
+    terztausch = 'A1 F G GG C'  # Dandrieu page 20, example in c major
+    terztausch_moll = 'A-1 F GG C'
     #mstr_minor = 'C1 D E- F G A B c c B- A- G F E- D C'
-    bass = m21.converter.parse('tinynotation: 4/4 ' + c_minor)
+    bass = m21.converter.parse('tinynotation: 4/4 ' + terztausch_moll)
     # bass = m21.converter.parse('c_major.musicxml')
 
     # get the list of notes
@@ -73,8 +94,9 @@ if __name__ == '__main__':
 
     # realizing the chords from the created figures
     fbRules = rules.Rules()  # in case you want to change the rules?
-    fbRules.partMovementLimits = [(1, 3), (2, 4), (3, 5)]
+    fbRules.partMovementLimits = [(1, 5), (2, 5), (3, 5)]
     allSols = fbLine.realize(fbRules)  # get all solutions
     print(allSols.getNumSolutions())
+    # allSols.generateAllRealizations().show()
     allSols.generateRandomRealization().show()  # generate a solution
 
